cbe97e09e116c9c9e2b348b781c508b7b9fb6dd3
Szymon <szymon.czyzmanski@gmail.com>
2021-05-26 21:29:21 +0200

    Removed server and tests

--- server/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "server.h"
-
-int main(int argc, char *argv[]) {
-    Server server{argc, argv};
-    server.start();
-    return 0;
-}
--- server/server.cpp
+++
@@ -1,9 +0,0 @@
-#include "server.h"
-
-Server::Server(int argc, const char **argv) {
-
-}
-
-void Server::start() {
-
-}
